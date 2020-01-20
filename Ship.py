698265eb369f7f200809b17270181d3c5376c411
SilversMind <silversmind@gmail.com>
2020-01-20 14:06:14 +0100

    Finished mechanism

--- Ship.py
+++ Ship.py
@@ -6,25 +6,51 @@ import random
 import Board
 
 class Ship:
-	def __init__(self, size=4):
+	def __init__(self, board, size=4):
 		self.size = size
 		self.position = list()
+		self.board = board
+
 
 	def choose_starting_square(self):
-		x, y = random.randrange(8), random.randrange(8)
+		x, y = random.randrange(self.board.size), random.randrange(self.board.size)
 		return x, y
 
-	def disposition(self, board):
+	def disposition_horizontally(self, board):
 		ship_placed = False
 		while not ship_placed:
 			self.position = list()
 			cpt = 0
 			x, y = self.choose_starting_square()
-			while not board.array_occupied[x][y] and cpt + y < board.size and cpt < self.size:
+			while cpt + y < self.board.size and not self.board.array_occupied[x][y + cpt] and cpt < self.size:
 				self.position.append((x, y + cpt))
 				cpt = cpt + 1
 			if cpt == self.size:
 				ship_placed = True
-				for x in self.position:
-					board.array_occupied[x[0]][x[1]] = True
+		for x in self.position:
+			self.board.array_occupied[x[0]][x[1]] = True
+
+	def disposition_vertically(self, board):
+		ship_placed = False
+		while not ship_placed:
+			self.position = list()
+			cpt = 0
+			x, y = self.choose_starting_square()
+			while cpt + x < self.board.size and not self.board.array_occupied[x + cpt][y] and cpt < self.size:
+				self.position.append((x + cpt, y))
+				cpt = cpt + 1
+			if cpt == self.size:
+				ship_placed = True
+		for x in self.position:
+			self.board.array_occupied[x[0]][x[1]] = True
+
+
+	def randomly_position_a_ship(self, board):
+		# CHoose randomly a value between 0 and 1 to choose whether to place the boat vertically or horizontally
+		position_number = random.randrange(2)
+		if position_number == 0:
+			self.disposition_horizontally(self.board)
+		else :
+			self.disposition_vertically(self.board)
+
 
--- test_Ship.py
+++ test_Ship.py
@@ -2,8 +2,8 @@ import unittest
 import Ship
 import Board
 
-b_test = Board.Board(8)
-ship_test = Ship.Ship(3)
+board_test = Board.Board(8)
+ship_test = Ship.Ship(board_test, 3)
 
 
 class test_Board(unittest.TestCase):
@@ -16,12 +16,31 @@ class test_Board(unittest.TestCase):
 		assert len(starting_square) == 2
 		assert all([True for x in starting_square if 0 < x < 8])
 
-	def test_disposition(self):
-		ship_test.disposition(b_test)
+	def test_disposition_horizontally(self):
+		b_test = Board.Board(8)
+		ship_test.disposition_horizontally(b_test)
 		assert len(ship_test.position) == 3
-		# Check if the board are all along the same column
+		# Check if the board are all along the same row
 		assert len(set([x[0] for x in ship_test.position])) == 1
 		# Check if the square are contiguous
 		assert ([x[1] for x in ship_test.position] == list(range(ship_test.position[0][1], ship_test.position[0][1] + ship_test.size)))
 
+	def test_disposition_vertically(self):
+		b_test = Board.Board(4)
+		ship_test.disposition_vertically(b_test)
+		assert len(ship_test.position) == 3
+		# Check if the board are all along the same column
+		assert len(set([x[1] for x in ship_test.position])) == 1
+		# Check if the square are contiguous
+		assert ([x[0] for x in ship_test.position] == list(range(ship_test.position[0][0], ship_test.position[0][0] + ship_test.size)))
+
+	def test_position_multiple_ships(self):
+		b_test = Board.Board(5)
+		size_ship_to_place = (4, 3, 3, 2, 2, 2, 1, 1, 1, 1)
+		for x in size_ship_to_place:
+			ship = Ship.Ship(b_test, x)
+			ship.randomly_position_a_ship(b_test)
+			# Check if the number of square occupied on the board is equal to the sum of the ships' size
+			# i.e check if all the ships have been placed
+		assert list(b_test.array_occupied.flatten()).count(True) == sum(size_ship_to_place)
 
